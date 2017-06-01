41ef4b5371dc446f305fd653909f1f596fdcb5a4
Visman <visman2008@googlemail.com>
2017-06-01 17:14:29 +0700

    Ticket #1101 SMTP Password field on admin_options page too short
    Удалил ограничение на длину пароля.
    https://fluxbb.org/development/core/tickets/1101/

--- admin_options.php
+++ admin_options.php
@@ -752,8 +752,8 @@ generate_admin_menu('options');
 									<td>
 										<label><input type="checkbox" name="form[smtp_change_pass]" value="1" />&#160;<?php echo $lang_admin_options['SMTP change password help'] ?></label>
 <?php $smtp_pass = !empty($pun_config['o_smtp_pass']) ? random_key(pun_strlen($pun_config['o_smtp_pass']), true) : ''; ?>
-										<input type="password" name="form[smtp_pass1]" size="25" maxlength="50" value="<?php echo $smtp_pass ?>" />
-										<input type="password" name="form[smtp_pass2]" size="25" maxlength="50" value="<?php echo $smtp_pass ?>" />
+										<input type="password" name="form[smtp_pass1]" size="25" value="<?php echo $smtp_pass ?>" />
+										<input type="password" name="form[smtp_pass2]" size="25" value="<?php echo $smtp_pass ?>" />
 										<span><?php echo $lang_admin_options['SMTP password help'] ?></span>
 									</td>
 								</tr>
