9237d961e4773735db8379285d4ec38bca0f8009
Zealot <zealot111@rambler.ru>
2017-10-08 18:14:29 +0300

    sqlite3 support
    sqlite3 support. merged from https://github.com/Koos10/fluxbb with some
    fixes from https://github.com/fluxbb/fluxbb (sqlite3 branch)

--- install.php
+++ install.php
@@ -229,6 +229,8 @@ if (!isset($_POST['form_sent']) || !empty($alerts))
 	}
 	if (function_exists('sqlite_open'))
 		$db_extensions[] = array('sqlite', 'SQLite');
+	if (class_exists('SQLite3'))
+		$db_extensions[] = array('sqlite3', 'SQLite3');
 	if (function_exists('pg_connect'))
 		$db_extensions[] = array('pgsql', 'PostgreSQL');
 
@@ -521,6 +523,10 @@ else
 			require PUN_ROOT.'include/dblayer/sqlite.php';
 			break;
 
+		case 'sqlite3':
+			require PUN_ROOT.'include/dblayer/sqlite3.php';
+			break;
+
 		default:
 			error(sprintf($lang_install['DB type not valid'], pun_htmlspecialchars($db_type)));
 	}
@@ -551,6 +557,7 @@ else
 			break;
 
 		case 'sqlite':
+		case 'sqlite3':
 			if (strtolower($db_prefix) == 'sqlite_')
 				error($lang_install['Prefix reserved']);
 			break;
@@ -1213,7 +1220,7 @@ else
 		)
 	);
 
-	if ($db_type == 'sqlite')
+	if ($db_type == 'sqlite' || $db_type == 'sqlite3')
 	{
 		$schema['PRIMARY KEY'] = array('id');
 		$schema['UNIQUE KEYS'] = array('word_idx'	=> array('word'));
