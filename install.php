0cfc2381d066124167d9820e58adc4a1d6033709
Visman <visman2008@googlemail.com>
2018-07-22 13:24:58 +0700

    Require at least PHP 5.6.12

--- install.php
+++ install.php
@@ -15,7 +15,7 @@ define('FORUM_DB_REVISION', 21);
 define('FORUM_SI_REVISION', 2.1);
 define('FORUM_PARSER_REVISION', 2);
 
-define('MIN_PHP_VERSION', '5.6.0');
+define('MIN_PHP_VERSION', '5.6.12');
 define('MIN_MYSQL_VERSION', '5.5.3');
 define('MIN_PGSQL_VERSION', '7.0.0');
 define('PUN_SEARCH_MIN_WORD', 3);
