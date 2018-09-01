8e050555f367cdc4718124347757c82e9b09f966
Visman <visman2008@googlemail.com>
2018-09-01 20:58:07 +0700

    Avoiding $db->num_rows() in install.php

--- install.php
+++ install.php
@@ -548,7 +548,7 @@ else
 
 	// Make sure FluxBB isn't already installed
 	$result = $db->query('SELECT 1 FROM '.$db_prefix.'users WHERE id=1');
-	if ($db->num_rows($result))
+	if ($db->result($result))
 		error(sprintf($lang_install['Existing table error'], $db_prefix, $db_name));
 
 	// Check if InnoDB is available
