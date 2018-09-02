78b202f56bee59348202010ad7754377cc9fb108
Visman <visman2008@googlemail.com>
2018-09-02 16:50:05 +0700

    Avoiding $db->num_rows() in plugins/AP_MergePosts.php

--- plugins/AP_MergePosts.php
+++ plugins/AP_MergePosts.php
@@ -45,13 +45,17 @@ else
 	if (!isset($pun_config['o_merge_timeout']))
 	{
 		$result = $db->query('SELECT conf_value FROM '.$db->prefix.'config WHERE conf_name=\'o_merge_timeout\'') or error('Unable to fetch config info', __FILE__, __LINE__, $db->error());
-		if (!$db->num_rows($result))
+		$row = $db->fetch_row($result);
+
+		if (is_array($row))
+		{
+			$merge_timeout = $row[0];
+		}
+		else
 		{
 			$db->query('INSERT INTO '.$db->prefix.'config (conf_name, conf_value) VALUES(\'o_merge_timeout\', \'86400\')') or error('Unable to insert into table config', __FILE__, __LINE__, $db->error());
 			$merge_timeout = '86400';
 		}
-		else
-			$merge_timeout = $db->result($result);
 
 		if (!defined('FORUM_CACHE_FUNCTIONS_LOADED'))
 			require PUN_ROOT.'include/cache.php';
