95890ad8920ab3096a706e3c231be820828274da
Visman <visman2008@googlemail.com>
2020-07-29 22:33:51 +0700

    Change language files of the Not Sum plugin

--- lang/English/admin_plugin_not_sum.php
+++ lang/English/admin_plugin_not_sum.php
@@ -2,10 +2,9 @@
 
 $lang_admin_plugin_not_sum = array(
 
-'Plugin title'	=>	'Not Sum - We switch off calculation of messages',
-'Explanation 1'			=>	'The plugin serves for deenergizing of calculation of messages of users in certain sections.',
-'Explanation 2'			=>	'In addition the plugin synchronizes quantity of messages at users.',
-'Show text button'		=>	'Save changes',
+'Plugin title'	=>	'Not to sum',
+'Explanation 1'			=>	'This plugin is used to turn off the counting of user messages in the marked forums and to recalculate the number of messages in users, regardless of changing settings.',
+'Show text button'		=>	'Recalculate and save',
 'Not Sum' => 'Not to sum',
 
 );
