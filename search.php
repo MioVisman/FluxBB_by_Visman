ac1c24373d40863d4105debf938487e2fc258a4e
Visman <visman2008@googlemail.com>
2021-07-20 19:45:51 +0700

    Fix for 8.1.0 Alpha 3

--- search.php
+++ search.php
@@ -56,8 +56,8 @@ if (isset($_GET['action']) || isset($_GET['search_id']))
 	// If it's a regular search (keywords and/or author)
 	else if ($action == 'search')
 	{
-		$keywords = (isset($_GET['keywords'])) ? utf8_strtolower(pun_trim($_GET['keywords'])) : null;
-		$author = (isset($_GET['author'])) ? utf8_strtolower(pun_trim($_GET['author'])) : null;
+		$keywords = isset($_GET['keywords']) ? utf8_strtolower(pun_trim($_GET['keywords'])) : '';
+		$author = isset($_GET['author']) ? utf8_strtolower(pun_trim($_GET['author'])) : '';
 
 		if (preg_match('%^[\*\%]+$%', $keywords) || (pun_strlen(str_replace(array('*', '%'), '', $keywords)) < PUN_SEARCH_MIN_WORD && !is_cjk($keywords)))
 			$keywords = '';
