2e544bc0ac5eef6aa48d15b76fecf9a499320dea
Visman <visman2008@googlemail.com>
2021-10-14 10:57:53 +0700

    Update AP_Upload.php

--- plugins/AP_Upload.php
+++ plugins/AP_Upload.php
@@ -13,7 +13,7 @@ if (! defined('PUN')) {
 
 // Tell admin_loader.php that this is indeed a plugin and that it is loaded
 define('PUN_PLUGIN_LOADED', 1);
-define('PLUGIN_VERSION', '3.2.0');
+define('PLUGIN_VERSION', '3.2.1');
 define('PLUGIN_URL', pun_htmlspecialchars('admin_loader.php?plugin=' . $plugin));
 define('PLUGIN_EXTS', 'webp,jpg,jpeg,png,gif,mp3,zip,rar,7z');
 define('PLUGIN_NF', 25);
@@ -148,10 +148,10 @@ else if (isset($_POST['update'])) {
 
 			foreach ($arr as $ext)
 			{
-				if (preg_match('%^[0-9a-z]+$%', $ext) && ! $upf_class->inBlackList($ext))
-					$g_ext[] = $ext;
-				else
+				if (preg_match('%[^0-9a-z]%', $ext) || $upf_class->inBlackList($ext))
 					$ext_bad[] = $ext;
+				else
+					$g_ext[] = $ext;
 			}
 
 			$g_ext = implode(',', $g_ext);
