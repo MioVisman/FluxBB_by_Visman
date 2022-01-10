73c538e45a2f9b381da1329752c2ed8398e3ecef
Visman <visman2008@googlemail.com>
2022-01-10 11:32:28 +0700

    Fix error() in dblayer

--- include/dblayer/mysqli.php
+++ include/dblayer/mysqli.php
@@ -171,7 +171,7 @@ class DBLayer
 
 	function error()
 	{
-		$result['error_sql'] = @current(@end($this->saved_queries));
+		$result['error_sql'] = empty($this->saved_queries) ? '' : end($this->saved_queries);
 		$result['error_no'] = $this->error_no;
 		$result['error_msg'] = $this->error_msg;
 
