1276b434870ae65f9368e9672e9f79b69e93dd78
Visman <visman2008@googlemail.com>
2022-12-03 10:46:01 +0700

    Update security_for_post.php

--- addons/security_for_post.php
+++ addons/security_for_post.php
@@ -118,8 +118,9 @@ class addon_security_for_post extends flux_addon
 			include PUN_ROOT.'include/security.php';
 
 		$now = time();
+		$form_key = $_POST[$this->form_key] ?? null;
 
-		if (!isset($_POST[$this->form_key]))
+		if (! is_string($form_key))
 		{
 			$errors[] = security_msg('1');
 			return;
@@ -128,7 +129,7 @@ class addon_security_for_post extends flux_addon
 		if (security_test_browser())
 			$errors[] = security_msg('2');
 
-		$result = $db->query('SELECT * FROM '.$db->prefix.'sec_of_post WHERE form_key=\''.$db->escape($_POST[$this->form_key]).'\' LIMIT 1') or error('Unable to get sec_of_post data', __FILE__, __LINE__, $db->error());
+		$result = $db->query('SELECT * FROM '.$db->prefix.'sec_of_post WHERE form_key=\''.$db->escape($form_key).'\' LIMIT 1') or error('Unable to get sec_of_post data', __FILE__, __LINE__, $db->error());
 		$cur_form = $db->fetch_assoc($result);
 
 		if (empty($cur_form['form_time']) || $cur_form['form_captcha'] == 'error')
@@ -155,9 +156,9 @@ class addon_security_for_post extends flux_addon
 		}
 
 		if (empty($errors))
-			$db->query('DELETE FROM '.$db->prefix.'sec_of_post WHERE form_key=\''.$db->escape($_POST[$this->form_key]).'\'') or error('Unable to delete sec_of_post data', __FILE__, __LINE__, $db->error());
+			$db->query('DELETE FROM '.$db->prefix.'sec_of_post WHERE form_key=\''.$db->escape($form_key).'\'') or error('Unable to delete sec_of_post data', __FILE__, __LINE__, $db->error());
 		else
-			$db->query('UPDATE '.$db->prefix.'sec_of_post SET form_captcha=\'error\' WHERE form_key=\''.$db->escape($_POST[$this->form_key]).'\'') or error('Unable to update sec_of_post data', __FILE__, __LINE__, $db->error());
+			$db->query('UPDATE '.$db->prefix.'sec_of_post SET form_captcha=\'error\' WHERE form_key=\''.$db->escape($form_key).'\'') or error('Unable to update sec_of_post data', __FILE__, __LINE__, $db->error());
 	}
 
 
