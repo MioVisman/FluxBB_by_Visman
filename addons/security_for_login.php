ae72dd2af6f0cffefd9bc5c03e1a8ed38381b095
Visman <visman2008@googlemail.com>
2022-12-03 10:45:54 +0700

    Update security_for_login.php

--- addons/security_for_login.php
+++ addons/security_for_login.php
@@ -119,8 +119,9 @@ class addon_security_for_login extends flux_addon
 			include PUN_ROOT.'include/security.php';
 
 		$now = time();
+		$form_key = $_POST[$this->form_key] ?? null;
 
-		if (!isset($_POST[$this->form_key]))
+		if (! is_string($form_key))
 		{
 			$errors[] = security_msg('1');
 			return;
@@ -132,7 +133,7 @@ class addon_security_for_login extends flux_addon
 		if (security_test_browser())
 			$errors[] = security_msg('2');
 
-		$result = $db->query('SELECT * FROM '.$db->prefix.'sec_of_login WHERE form_key=\''.$db->escape($_POST[$this->form_key]).'\' LIMIT 1') or error('Unable to get sec_of_login data', __FILE__, __LINE__, $db->error());
+		$result = $db->query('SELECT * FROM '.$db->prefix.'sec_of_login WHERE form_key=\''.$db->escape($form_key).'\' LIMIT 1') or error('Unable to get sec_of_login data', __FILE__, __LINE__, $db->error());
 		$cur_form = $db->fetch_assoc($result);
 
 		if (empty($cur_form['form_time']) || $cur_form['form_captcha'] == 'error')
@@ -158,9 +159,9 @@ class addon_security_for_login extends flux_addon
 		}
 
 		if (empty($errors))
-			$db->query('DELETE FROM '.$db->prefix.'sec_of_login WHERE form_key=\''.$db->escape($_POST[$this->form_key]).'\'') or error('Unable to delete sec_of_login data', __FILE__, __LINE__, $db->error());
+			$db->query('DELETE FROM '.$db->prefix.'sec_of_login WHERE form_key=\''.$db->escape($form_key).'\'') or error('Unable to delete sec_of_login data', __FILE__, __LINE__, $db->error());
 		else
-			$db->query('UPDATE '.$db->prefix.'sec_of_login SET form_captcha=\'error\' WHERE form_key=\''.$db->escape($_POST[$this->form_key]).'\'') or error('Unable to update sec_of_login data', __FILE__, __LINE__, $db->error());
+			$db->query('UPDATE '.$db->prefix.'sec_of_login SET form_captcha=\'error\' WHERE form_key=\''.$db->escape($form_key).'\'') or error('Unable to update sec_of_login data', __FILE__, __LINE__, $db->error());
 	}
 
 
