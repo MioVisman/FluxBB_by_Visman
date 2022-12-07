20167c71bab741a27749a8aad6653090dd6b9e13
Visman <visman2008@googlemail.com>
2022-12-07 16:31:04 +0700

    Update fancybox.php

--- include/fancybox.php
+++ include/fancybox.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (C) 2011-2015 Visman (mio.visman@yandex.ru)
+ * Copyright (C) 2011-2022 Visman (mio.visman@yandex.ru)
  * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  */
 
@@ -17,15 +17,7 @@ if (isset($pun_config['o_fbox_files']) && !isset($http_status) && (!$pun_user['i
 
 		$page_head['fancyboxcss'] = '<link rel="stylesheet" type="text/css" href="style/imports/fancybox.css" />';
 
-		if (!isset($page_js))
-		{
-			$page_head['jquery'] = '<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>';
-			$page_head['fancybox'] = '<script type="text/javascript" src="js/fancybox.js"></script>';
-		}
-		else // For FluxBB by Visman
-		{
-			$page_js['j'] = 1;
-			$page_js['f']['fancybox'] = 'js/fancybox.js';
-		}
+		$page_js['j'] = 1;
+		$page_js['f']['fancybox'] = 'js/fancybox.js';
 	}
 }
